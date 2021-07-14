7c901b7b5f5c8a62102bfd2932c8e304371beb1c
beerkaya <beerkaya@github.com>
2021-07-14 22:33:21 +0300

    Add project files.

---
+++ MyBooleanOutput/MyBooleanOutput.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyBooleanOutputTester
+{
+    public partial class MyBooleanOutput : Component
+    {
+        public MyBooleanOutput()
+        {
+            
+        }
+
+        public MyBooleanOutput(IContainer container)
+        {
+            container.Add(this);
+        }
+
+        #region Properties
+
+        public bool Result { get; private set; }
+        public string Equation { private get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public void SetEq(string equation)
+        {
+            this.Equation = equation;
+        }
+
+        public void SetParameters(string parameterNames ,params int[] parameterValues)
+        {
+            for (int i = 0; i < parameterNames.Length; i++)
+            {
+                Equation = Equation.Replace(parameterNames[i], Convert.ToChar(parameterValues[i].ToString()));
+            }
+        }
+
+        public bool GetResult()
+        {
+            string result = Postfix(Equation);
+            return result == "1";
+        }
+
+        public bool GetResult(string equation)
+        {
+            string result = Postfix(equation);
+            return result == "1";
+        }
+
+        private string Postfix(string infix)
+        {
+            if (infix.Contains('(') && infix.Contains(')'))
+            {
+                Regex regex = new Regex(@"\([\d\.]+\)");
+                MatchCollection collection = regex.Matches(infix);
+                for (int i = 0; i < collection.Count; i++)
+                {
+                    string replacement = infix.Substring(collection[0].Index + 1, collection[0].Value.Length - 2);
+                    infix = infix.Replace($"({replacement})", Postfix(replacement));
+                    collection = regex.Matches(infix);
+                }
+            }
+            else if (infix.Contains('.') && infix.Contains('+'))
+                throw new Exception("İki ayrı operator kullanılıyorsa parantez kullanılmalıdır.");
+
+            Stack<int> numbers = new();
+            Stack<char> operators = new();
+
+            foreach (var token in infix)
+            {
+                if (char.IsDigit(token)) numbers.Push(token);
+                else if (IsOperator(token)) operators.Push(token);
+            }
+
+            return Calculate(numbers, operators);
+        }
+        private string Calculate(Stack<int> numbers, Stack<char> operators)
+        {
+            int res = numbers.Pop();
+            for (int i = 0; i < numbers.Count + operators.Count; i++)
+            {
+                char op = operators.Pop();
+
+                if (op == '+')
+                {
+                    int num1 = numbers.Pop();
+                    res = num1 == 49 || res == 49 ? 49 : 48;
+                }
+                else if (op == '.')
+                {
+                    int num1 = numbers.Pop();
+                    res = num1 == 49 && res == 49 ? 49 : 48;
+                }
+            }
+            return ((char)res).ToString();
+        }
+        private bool IsOperator(char opt)
+        {
+            string operators = "+.";
+            return operators.Contains(opt);
+        }
+        #endregion
+    }
+}
---
+++ MyBooleanOutput/Tester.Designer.cs
@@ -0,0 +1,53 @@
+﻿
+namespace MyBooleanOutputTester
+{
+    partial class Tester
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myBooleanOutput1 = new MyBooleanOutputTester.MyBooleanOutput(this.components);
+            this.SuspendLayout();
+            // 
+            // Tester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Tester";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Tester_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyBooleanOutput myBooleanOutput1;
+    }
+}
+
